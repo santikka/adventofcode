eeec51a0a9daca280bad9a76f175d85c083fb661
santikka <santtu.tikka@jyu.fi>
2025-12-26 11:27:30 +0200

    2025 day 12

--- 2025/day12.R
+++ 2025/day12.R
@@ -9,64 +9,12 @@ shapes <- lapply(which(new_shape), function(i) shape_txt[(i + 1):(i + 3)]) |>
       y <- do.call("rbind", strsplit(x, ""))
       idx_mat <- which(y == "#", arr.ind = TRUE)
       dimnames(idx_mat) <- NULL
-      # convert index into coordinates with the center as the origin
       idx_mat[, 1] <- 2 - idx_mat[, 1]
       idx_mat[, 2] <- idx_mat[, 2] - 2
       idx_mat <- idx_mat[, c(2, 1)]
       idx_mat[order(idx_mat[, 1], idx_mat[, 2]), ]
     }
   )
-
-plot_shape <- function(mat) {
-  row <- 2 - mat[, 2]
-  col <- 2 + mat[, 1]
-  new <- matrix(".", 3, 3)
-  new[cbind(row, col)] <- "#"
-  print(new)
-}
-
-flip_h <- function(mat) {
-  mat[, 2] <- -1 * mat[, 2]
-  mat[order(mat[, 1], mat[, 2]), ]
-}
-
-flip_v <- function(mat) {
-  mat[, 1] <- -1 * mat[, 1]
-  mat[order(mat[, 1], mat[, 2]), ]
-}
-
-rotate <- function(mat, times = 1) {
-  rot <- matrix(c(0, 1, -1, 0), 2, 2)
-  out <- mat
-  for (i in 1:times) {
-    out <- out %*% rot
-  }
-  out[order(out[, 1], out[, 2]), ]
-}
-
-all_shapes <- lapply(
-  shapes,
-  function(x) {
-    vx <- flip_v(x)
-    hx <- flip_h(x)
-    unique(
-      list(
-        x,
-        rotate(x, 1),
-        rotate(x, 2),
-        rotate(x, 3),
-        vx,
-        rotate(vx, 1),
-        rotate(vx, 2),
-        rotate(vx, 3),
-        hx,
-        rotate(hx, 1),
-        rotate(hx, 2),
-        rotate(hx, 3)
-      )
-    )
-  }
-)
 
 regions <- data[(idx + 1):length(data)] |>
   strsplit(split = ":")
@@ -76,9 +24,10 @@ quantities <- lapply(regions, "[[", 2) |>
   lapply(function(x) as.integer(strsplit(trimws(x), " ")[[1]]))
 
 # Part 1
-shape_sizes <- vapply(all_shapes, function(x) nrow(x[[1]]), integer(1))
+shape_sizes <- vapply(shapes, nrow, integer(1))
 areas <- vapply(rectangles, prod, double(1))
-totals <- vapply(quantities, function(x) sum(x * shape_sizes), integer(1L))
-test_fit <- which(totals <= areas)
-
-# Part 2
+totals <- vapply(quantities, function(x) sum(x * shape_sizes), integer(1))
+viable <- which(totals <= areas)
+naive_totals <- vapply(quantities, function(x) sum(x * 9L), integer(1))
+result <- sum(naive_totals[viable] <= areas[viable])
+result
